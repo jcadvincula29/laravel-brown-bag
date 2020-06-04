0f77de13b17df1c5df75d7ca9c306a7a2175cdfc
John Carlo <john.advincula@bizmates.ph>
2020-06-04 12:04:56 +0800

    add test and endpoint for fetching specific task

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -51,7 +51,7 @@ class TodoController extends Controller
      */
     public function show(Todo $todo)
     {
-        //
+        return response()->json($todo);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/todos', 'TodoController@store');
+Route::get('/todos/{todo}', 'TodoController@show');
